671441a4f9f3f9a91291250e31ac057a8c004d43
dovahlana <atennison25@gmail.com>
2020-01-08 19:36:57 -0600

    actual commit

--- practice.js
+++ practice.js
@@ -27,7 +27,10 @@
   Then invoke the callback function, passing in the first element in the array as it's argument.
 */
 
-// Code Here 
+function first(arr,cb) {
+  cb(arr[0])
+
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -47,7 +50,9 @@ first(names, function(firstName){
   Then invoke the callback, passing in the last element in the array as the argument.
 */
 
-//Code Here
+function last(arr, cb) {
+  cb(arr[arr.length - 1])
+}
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -65,7 +70,11 @@ last(names, function(lastName){
   Invoke the callback, passing in the product of the two numbers multiplied as the argument. 
 */
 
-//Code Here
+function multiply (a,b,c) {
+
+
+c(a*b)
+}
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -84,7 +93,15 @@ multiply(4, 3, function(answer){
   If the name does not exist, invoke the callback with false as the argument.
 */
 
-//Code Here 
+function contains(arr,str,cb) {
+  if (arr.includes(str)) {
+    cb(true); }
+      else {
+        cb(false);
+      }
+
+}
+
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -105,7 +122,7 @@ contains(names, 'Colt', function(result){
   Remove any duplicate values from the array, and invoke the callback with the modified array as an argument.
 */
 
-//Code Here
+
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
@@ -122,7 +139,7 @@ uniq(names, function(uniqArr){
   For each name in the array, invoke the callback and pass in the name and the name's index as arguments.
 */
 
-//Code Here 
+
 
 // Do not edit the code below.
 each(names, function(item, indice){
